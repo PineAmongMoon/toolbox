ff880d5e592e5dae638130fcc730a6a7a8e90933
PineAmongMoon <juyuanmao@outlook.com>
2023-02-09 15:07:01 +0800

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 export default function HomePage() {
   return (
-    <div>
-      <h2>这是一个简单的工具箱</h2>
-    </div>
+    <>
+      <div>
+        <h2>这是一个简单的工具箱</h2>
+      </div>
+      <div>
+        <h3>隐私政策:本网站不会记录或上传任何用户信息</h3>
+      </div>
+      <div>
+        <h3>本网站所有源代码托管在Github上, 遵循MIT许可</h3>
+      </div>
+    </>
   );
 }
